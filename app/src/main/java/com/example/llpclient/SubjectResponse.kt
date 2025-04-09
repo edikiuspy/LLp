8dc6b16a60bc99702a40c6c77d623380b6af8536
edikiuspy <u96_edumun_waw@technischools.com>
2025-04-09 11:23:05 +0200

    small fix

--- app/src/main/java/com/example/llpclient/SubjectResponse.kt
+++ app/src/main/java/com/example/llpclient/SubjectResponse.kt
@@ -6,3 +6,8 @@ data class SubjectResponse(
     @SerializedName("Subject") val subject: SubjectDto
 )
 
+
+data class SubjectDto(
+    @SerializedName("Id") val id: Int,
+    @SerializedName("Name") val name: String
+)
